c8cadb17a45cc6a01ebb0836e3515b1556ab725a
d4rkfly3r <npjoshf@gmail.com>
2015-08-18 14:43:12 -0700

    Fixed a stupid foldername

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -2,10 +2,6 @@
 
 use Helpers\Assets;
 use Helpers\Url;
-use Helpers\Hooks;
-
-//initialise hooks
-$hooks = Hooks::get();
 ?>
 
 </div>
@@ -17,11 +13,6 @@ Assets::js(array(
 	'//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'
 ));
 
-//hook for plugging in javascript
-$hooks->run('js');
-
-//hook for plugging in code into the footer
-$hooks->run('footer');
 ?>
 
 </body>
--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -2,10 +2,6 @@
 
 use Helpers\Assets;
 use Helpers\Url;
-use Helpers\Hooks;
-
-//initialise hooks
-$hooks = Hooks::get();
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo LANGUAGE_CODE; ?>">
@@ -13,10 +9,6 @@ $hooks = Hooks::get();
 
 	<!-- Site meta -->
 	<meta charset="utf-8">
-	<?php
-	//hook for plugging in meta tags
-	$hooks->run('meta');
-	?>
 	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in app/Core/Config.php ?></title>
 
 	<!-- CSS -->
@@ -26,15 +18,9 @@ $hooks = Hooks::get();
 		Url::templatePath() . 'css/style.css',
 	));
 
-	//hook for plugging in css
-	$hooks->run('css');
 	?>
 
 </head>
 <body>
-<?php
-//hook for running code after body tag
-$hooks->run('afterBody');
-?>
 
 <div class="container">
